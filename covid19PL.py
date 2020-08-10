68a042ccb268a7e8dfae65121a50e8ac35739ca0
Jelly12301 <50546844+Jelly12301@users.noreply.github.com>
2020-08-10 21:45:11 +0200

    Create covid19PL.py

---
+++ covid19PL.py
@@ -0,0 +1,7 @@
+import json,urllib.request
+data = urllib.request.urlopen("https://api.covid19api.com/dayone/country/poland").read()
+output = json.loads(data)
+ilosckoronawirusow = output[len(output) - 1]["Confirmed"]
+koronawirusydata = output[len(output) - 1]["Date"]
+iloscnowa = output[len(output) - 1]["Confirmed"] - output[len(output) - 2]["Confirmed"]
+print("W dniu", koronawirusydata, "ilość aktywnych koronaświrusów wynosi", ilosckoronawirusow, "przybyło nowych", iloscnowa, "koronaświrusów w Polsce.")
