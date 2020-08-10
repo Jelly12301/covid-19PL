5609f38eadad41b1b3a49407a38805c5c2add7da
Jelly12301 <50546844+Jelly12301@users.noreply.github.com>
2020-08-10 21:49:05 +0200

    Add files via upload

--- covid19PL.py
+++ covid19PL.py
@@ -4,4 +4,4 @@ output = json.loads(data)
 ilosckoronawirusow = output[len(output) - 1]["Confirmed"]
 koronawirusydata = output[len(output) - 1]["Date"]
 iloscnowa = output[len(output) - 1]["Confirmed"] - output[len(output) - 2]["Confirmed"]
-print("W dniu", koronawirusydata, "ilość aktywnych koronaświrusów wynosi", ilosckoronawirusow, "przybyło nowych", iloscnowa, "koronaświrusów w Polsce.")
+print("W dniu", koronawirusydata, "ilość aktywnych koronawirusów wynosi", ilosckoronawirusow, "przybyło nowych", iloscnowa, "koronaświrusów w Polsce.")
