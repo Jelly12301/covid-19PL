0d9d88ec7768327beea818054f6884c8c0cf4b84
Jelly12301 <50546844+Jelly12301@users.noreply.github.com>
2020-08-12 22:14:27 +0200

    Add files via upload

--- covid19PL.py
+++ covid19PL.py
@@ -1,7 +1,10 @@
 import json,urllib.request
-data = urllib.request.urlopen("https://api.covid19api.com/dayone/country/poland").read()
+req = urllib.request.Request(url="https://api.nowywirus.pl/", headers={'User-Agent':' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'})
+data = urllib.request.urlopen(req).read()
 output = json.loads(data)
-ilosckoronawirusow = output[len(output) - 1]["Confirmed"]
-koronawirusydata = output[len(output) - 1]["Date"]
-iloscnowa = output[len(output) - 1]["Confirmed"] - output[len(output) - 2]["Confirmed"]
-print("W dniu", koronawirusydata, "ilość aktywnych koronawirusów wynosi", ilosckoronawirusow, "przybyło nowych", iloscnowa, "koronawirusów w Polsce.")
+ilosckoronawirusow = output[0]["cases"]
+iloscnowa = output[0]["new_cases"]
+deaths = output[0]["deaths"]
+deathstoday = output[0]["new_deaths"]
+print(f"Ilość aktywnych koronawirusów wynosi {ilosckoronawirusow}, przybyło nowych {iloscnowa} koronawirusów w Polsce.")
+print(f"Nie żyje już {deaths} osób, dzisiaj zmarło {deathstoday}.")
