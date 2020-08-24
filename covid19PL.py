5f970fcaba285f7c23a0674f64458eb1531da303
Dawid Banaszewski <dbanaszewski@onet.pl>
2020-08-24 19:50:24 +0200

    Change API

--- covid19PL.py
+++ covid19PL.py
@@ -1,10 +1,13 @@
 import json,urllib.request
-req = urllib.request.Request(url="https://api.nowywirus.pl/", headers={'User-Agent':' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'})
+token = "TUTAJ_TOKEN"
+req = urllib.request.Request(url=f"https://www.dbanaszewski.com/api/covid/?token={token}")
 data = urllib.request.urlopen(req).read()
 output = json.loads(data)
-ilosckoronawirusow = output[0]["cases"]
-iloscnowa = output[0]["new_cases"]
-deaths = output[0]["deaths"]
-deathstoday = output[0]["new_deaths"]
-print(f"Ilość aktywnych koronawirusów wynosi {ilosckoronawirusow}, przybyło nowych {iloscnowa} koronawirusów w Polsce.")
-print(f"Nie żyje już {deaths} osób, dzisiaj zmarło {deathstoday}.")
+ilosckoronawirusow = output["all_infected"]
+iloscnowa = output["infected"]
+deaths = output["all_death"]
+deathstoday = output["death"]
+today = output["date"]
+print(f"Statystyki na dzień {today}:")
+print(f"  - ilość aktywnych koronawirusów wynosi {ilosckoronawirusow}, przybyło nowych {iloscnowa} koronawirusów w Polsce.")
+print(f"  - nie żyje już {deaths} osób, dzisiaj zmarło {deathstoday}.")
