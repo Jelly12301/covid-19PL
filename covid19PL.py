f30b073a311d29677a709d6a7dfd632a2ae15396
Jelly12301 <50546844+Jelly12301@users.noreply.github.com>
2020-10-15 17:32:31 +0200

    Add files via upload

--- covid19PL.py
+++ covid19PL.py
@@ -1,17 +1,12 @@
 import json,urllib.request
-
-
-
-
 req = urllib.request.Request(url="https://nowywirus.pl/api/", headers={'User-Agent':' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'})
 data = urllib.request.urlopen(req).read()
 output = json.loads(data)
 cases = output[0]["cases"]
-
 new_cases = output[0]["new_cases"]
 deaths = output[0]["deaths"]
 new_deaths = output[0]["new_deaths"]
-print("Aktualne dane o covid-19.")
+print("Aktualne dane o covid-19 w polsce.")
 print(f"Ilość potwierdzonych przypadków: {cases}.")
 print(f"Ilość nowych zakażeń: {new_cases}.")
 print(f"Zgony: {deaths}.")
